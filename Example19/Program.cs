f252471fae039b0575da6dafbebc2a79559376d6
Stanislav <avgustwot@mail.ru>
2023-03-16 01:07:09 +0300

    Задача 19 исправленная

--- Example19/Program.cs
+++ Example19/Program.cs
@@ -8,13 +8,15 @@
 
 
 Console.Clear();
-// Console.WriteLine("Введите число:");
-// int n=Convert.ToInt32(Console.ReadLine());
-int n = new Random().Next(10000, 100000);
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+//int n = new Random().Next(10000, 100000);
 Console.WriteLine($"Число: {n}");
-int n1 = n / 1000;
-int n2 = n % 100;
-if (n1 == n2)
+int n1 = n / 10000;
+int n2 = n / 1000 % 10;
+int n3 = n % 100 / 10;
+int n4 = n % 10;
+if (n2 == n3 && n1 == n4)
     Console.WriteLine("Это число является палиндромом");
 else
     Console.WriteLine("Это чиcло не является палиндроном");
